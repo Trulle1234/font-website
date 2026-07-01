50ba976c0711e229eecf89d011fb642e1a3ab8a9
Trulle123 <leonard.trulsson@gmail.com>
2026-07-02 00:08:34 +0700

    add src images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+    const images = import.meta.glob("/src/lib/src_imgs/*", { eager: true, query: "?url", import: "default" });
+    const srcImgs: string[] = Object.values(images) as string[];
+</script>
+
 <h1>How I made [Font name]</h1>
-<p class="page-link"><a href="/font">Get the font</a></p>
+<p class="page-link"><a href="/font">Get the font</a> <a href="#src-imgs">See source images</a></p>
+
 
 <div class="main-container">
     <div class="main-content">
@@ -8,12 +14,15 @@
             and I got the idea to take photos on my trip to Thailand and make a font by using 
             a bunch of diffrent letters from diffrent stuff.</p>
 
+        <hr>
+
         <h2>How?</h2>
         <p>
             I took photos on my way to and in Thailand. 
             Then I cut them out, made them B&W and added them to a 
             <a href="https://www.calligraphr.com/">Calligraphr</a> template in Affinity.
         </p>
+
         <strong>That process looked something like this:</strong>
             <ol>
                 <li>
@@ -36,5 +45,16 @@
                     <em>When that has been done for all charachters. Upload to Calligraphr.</em>
                 </li>
             </ol>
+
+        <hr>
+
+        <h2 id="src-imgs">Source images <em>(Unorderd)</em></h2>
+        
+        <div class="src-imgs-container">
+            {#each srcImgs as img}
+                <img src={img} alt="1" class="src-img">
+            {/each}
+        </div>
+
     </div>
 </div>
